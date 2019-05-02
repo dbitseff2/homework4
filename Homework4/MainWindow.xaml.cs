d3562d83e0c4fb664e196084ef410a6491604db8
Dan Bitseff <dbitseff@gmail.com>
2019-05-01 22:25:39 -0700

    homework4

---
+++ Homework4/MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+namespace Homework4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string _usZipRegEx = @"^\d{5}(?:[-\s]\d{4})?$";
+        string _caZipRegEx = @"^([ABCEGHJKLMNPRSTVXY]\d[ABCEGHJKLMNPRSTVWXYZ])\ {0,1}(\d[ABCEGHJKLMNPRSTVWXYZ]\d)$";
+        public MainWindow()
+        {
+            InitializeComponent();
+            uxSubmit.IsEnabled = false;
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {            
+            TextBox textBox1 = (TextBox)sender;           
+            string text = textBox1.Text + e.Text.ToString();
+            
+            if (IsUSOrCanadianZipCode(text))
+                uxSubmit.IsEnabled = true;
+            else
+                uxSubmit.IsEnabled = false;
+        }
+
+        private bool IsUSOrCanadianZipCode(string zipCode)
+        {
+            var validZipCode = true;
+            if ((!Regex.Match(zipCode, _usZipRegEx).Success) && (!Regex.Match(zipCode, _caZipRegEx).Success))
+            {
+                validZipCode = false;
+            }
+            return validZipCode;
+        }
+
+        private void UxSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("submitted");
+        }
+
+        private void KeyDownValidation(object sender, KeyEventArgs e)
+        {
+            var textBox = sender as TextBox;            
+
+            string proposedText = null;
+            
+            if (e.Key == Key.Space)
+            {
+                proposedText = GetProposedText(textBox, " ");
+            }            
+            else if (e.Key == Key.Back)
+            {
+                proposedText = GetProposedTextBackspace(textBox);
+            }
+            if (proposedText != null && IsUSOrCanadianZipCode(proposedText))
+            {
+                uxSubmit.IsEnabled = true;
+            }
+            else
+            {
+                uxSubmit.IsEnabled = false;
+            }
+        }
+
+        private static string GetProposedTextBackspace(TextBox textBox)
+        {
+            var text = GetTextWithSelectionRemoved(textBox);
+            if (textBox.SelectionStart > 0 && textBox.SelectionLength == 0)
+            {
+                text = text.Remove(textBox.SelectionStart - 1, 1);
+            }
+
+            return text;
+        }
+
+        private static string GetProposedText(TextBox textBox, string newText)
+        {
+            var text = GetTextWithSelectionRemoved(textBox);
+            text = text.Insert(textBox.CaretIndex, newText);
+
+            return text;
+        }
+
+        private static string GetTextWithSelectionRemoved(TextBox textBox)
+        {
+            var text = textBox.Text;
+
+            if (textBox.SelectionStart != -1)
+            {
+                text = text.Remove(textBox.SelectionStart, textBox.SelectionLength);
+            }
+            return text;
+        }
+    }
+}
